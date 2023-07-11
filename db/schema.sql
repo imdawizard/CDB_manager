4f1f8bde3d73554939b03c2cc194cf9d88c0806c
Spyrodragonman Mannyman <nate.is.cool.yeah@gmail.com>
2023-07-11 17:44:29 -0600

    added three tables(department, role, employee) in schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_CMS_db;
+CREATE DATABASE employee_CMS_db;
+
+USE employee_CMS_db;
+
+CREATE TABLE department (
+    id INT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
